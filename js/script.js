119bff9b5d4b5d7834a3235e895d380e89377997
pon08Oa <87184564+pon08Oa@users.noreply.github.com>
2022-11-10 18:52:37 +0900

    btn3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const btn = document.querySelector('.btn-menu');
+const btn = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
 
 btn.addEventListener('click',() => {
     nav.classList.toggle('open-menu')
     if (btn.innerHTML === 'メニュー') {
         btn.innerHTML = '閉じる';
-    } else {
+    } 
+    else {
         btn.innerHTML = 'メニュー';
     }
 });
